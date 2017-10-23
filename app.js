e2c8bde334058f0f8404dfb7cbe75b22d7d55dbc
Daniel Rigberg <daniel.rigberg@gmail.com>
2017-10-23 14:30:16 +0200

    serve static html

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require("express")
 const app = express()
+const path = require("path")
 const port = 9001
-app.set("view engine", "ejs")
-app.set("views", "./views")
 
+app.use(express.static('views'))
 
 app.get('/', function (req, res) {
-  res.render("personalsite")
+  res.sendFile(path.join(__dirname, 'views/personalsite.html'));
 })
 
-app.listen(port, function(){
+app.listen(port, function() {
   console.log("Personal site server is running on port " + port);
 });
